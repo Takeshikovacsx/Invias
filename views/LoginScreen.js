53d20683656e7b668d79cff436f413303b1d04c0
Takeshikovacsx <hannerhd1@gmail.com>
2024-12-06 06:54:18 -0500

    PULL

--- views/LoginScreen.js
+++ views/LoginScreen.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
-import Icon from 'react-native-vector-icons/Ionicons';
 import * as Animatable from 'react-native-animatable'; // Importamos para usar animaciones
 
 const LoginScreen = () => {
